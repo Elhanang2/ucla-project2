7b45bd7a8562ded8fe9bbd8d002d0c542a0adfab
Stephen Pellegrini <spellegrini156@gmail.com>
2018-09-11 22:18:54 -0700

    reorganized sql folder

--- sql/seedTestRecommendations.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO recommendations (text, category, min_score, max_score)
-VALUES ('test', 'habit', 50, 100);
-INSERT INTO recommendations (text, category, min_score, max_score)
-VALUES ('test2', 'energy', 40, 60);
--- sql/seedsTestUserRecs.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO userrecommendations (RecommendationId, UserId) 
-Values (1, 2);
-INSERT INTO userrecommendations (RecommendationId, UserId) 
-Values (1, 1);
-INSERT INTO userrecommendations (RecommendationId, UserId) 
-Values (2, 2);
-INSERT INTO userrecommendations (RecommendationId, UserId) 
-Values (2, 1);
